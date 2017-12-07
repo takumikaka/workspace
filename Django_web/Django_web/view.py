34c166f954847450ab8646c9ac0d7aa46b33fac8
takumikaka <qk2006qk@163.com>
2017-12-07 14:05:48 +0800

    add django modle

--- Django_web/Django_web/view.py
+++ Django_web/Django_web/view.py
@@ -1,7 +1,10 @@
 #!/use/bin/python
 # -*- coding: utf-8 -*-
 
-from django.http import HttpResponse
+from django.shortcuts import render
+#from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hello World! ")
+    context = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'hello.html', context)
