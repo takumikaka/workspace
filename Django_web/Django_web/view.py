e4b4d880876cb465895b7749f18f2ca9b5a13667
takumikaka <qk2006qk@163.com>
2017-12-07 14:09:18 +0800

    add test words

--- Django_web/Django_web/view.py
+++ Django_web/Django_web/view.py
@@ -6,5 +6,5 @@ from django.shortcuts import render
 
 def hello(request):
     context = {}
-    context['hello'] = 'Hello World!'
+    context['hello'] = 'This is test words!'
     return render(request, 'hello.html', context)
