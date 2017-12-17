30d336c2bd8a4a35eacd23eed69c616541f1c626
takumikaka <qk2006qk@163.com>
2017-12-17 15:51:26 +0800

    some files about django shell

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysql',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': 'raspberry',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
