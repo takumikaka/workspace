aa7809e551f7fc8b9d1f410116f7bbc2cf6ff2c0
takumikaka <qk2006qk@163.com>
2017-12-25 11:31:16 +0800

    modify admin.py add Choice

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 
@@ -10,3 +10,4 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
