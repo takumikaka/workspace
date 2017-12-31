c900417b353ec3a311dc960264ade52d96fca73d
takumikaka <qk2006qk@163.com>
2018-01-01 00:00:07 +0800

    学习bootstrap了解页面布局

--- jiraproject/jira/views.py
+++ jiraproject/jira/views.py
@@ -4,6 +4,6 @@ from .models import Bugcontext
 
 # Create your views here.
 
-def bug_title(request):
-    bug_titles = Bugcontext.objects.all()
-    return render(request, "jira/titles.html", {"bug_titles": bug_titles})
+def jira_title(request):
+    jiras = Bugcontext.objects.all()
+    return render(request, "jira/titles.html", {"jiras": jiras})
