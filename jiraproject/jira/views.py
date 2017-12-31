570696fb40768443b7b4ff178e4ba8f26ca1aa0c
takumikaka <qk2006qk@163.com>
2017-12-31 20:08:21 +0800

    给jira模板增加视图

--- jiraproject/jira/views.py
+++ jiraproject/jira/views.py
@@ -4,12 +4,6 @@ from .models import Bugcontext
 
 # Create your views here.
 
-def index(request):
-    bug_list = Bugcontext.objects.all().order_by('-created_time')
-    return render(request, 'jira/index.html', context={'bug_list':bug_list})
-    '''
-    return render(request, "jira/index.html", context={
-                        'title': 'JIRA',
-                        'welcome': 'This is Jira.'
-})
-    '''
+def bug_title(request):
+    bug_titles = Bugcontext.objects.all()
+    return render(request, "jira/titles.html", {"bug_titles": bug_titles})
