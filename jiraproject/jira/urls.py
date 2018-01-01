683ad9bc59eaa11065ba1865ce1a8dbfe667d1bf
takumikaka <qk2006qk@163.com>
2018-01-01 15:52:31 +0800

    完成最简单的jira模板

--- jiraproject/jira/urls.py
+++ jiraproject/jira/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.jira_title, name='jira_title'),
+    url(r'(?P<context_id>\d)/$', views.jira_context, name="jira_context"),
 ]
--- jiraproject/jira/views.py
+++ jiraproject/jira/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Bugcontext
 
@@ -7,3 +7,9 @@ from .models import Bugcontext
 def jira_title(request):
     jiras = Bugcontext.objects.all()
     return render(request, "jira/titles.html", {"jiras": jiras})
+
+def jira_context(request, context_id):
+    #context = Bugcontext.objects.get(id = context_id)
+    context = get_object_or_404(Bugcontext, id=context_id)
+    pub = context.publish
+    return render(request, "jira/content.html", {"context": context, "publish": pub})
