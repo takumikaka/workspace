a8945bf2173630b5441953963d6795d000751f50
takumikaka <qk2006qk@163.com>
2018-01-02 20:21:36 +0800

    complete practice part one

--- practice/mysite/blog/admin.py
+++ practice/mysite/blog/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from blog.models import BlogArticles
 
 # Register your models here.
-admin.site.register(BlogArticles)
+
+class BlogArticlesAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "body", "publish")
+    list_filter = ("publish", "author")
+    search_fields = ("title", "body")
+    date_hierarchy = "publish"
+    ordering = ["publish", "author"]
+
+admin.site.register(BlogArticles, BlogArticlesAdmin)
--- practice/mysite/blog/views.py
+++ practice/mysite/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import BlogArticles
 
 # Create your views here.
+
+def blog_title(request):
+    blogs = BlogArticles.objects.all()
+    return render(request, "blog/titles.html", {"blogs": blogs})
