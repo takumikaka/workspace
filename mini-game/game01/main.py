4fcba889f55b4f4b4262cb695dcfc831e69740a2
takumikaka <qk2006qk@163.com>
2018-01-04 20:03:23 +0800

    update mini-game01

--- mini-game/game01/main.py
+++ mini-game/game01/main.py
@@ -14,26 +14,46 @@ def yellow(text):
 #############################################################################
 
 class Action(object):
-    
+
     def __init__(self):
         pass
-    
+
     def _Base_List(self):
         list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
                 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
                 'w', 'x', 'y', 'z',
         ]
         return list
-    
+
+    def _Base_dict(self):
+        dict = {"dag": "a person who does not look attractive or who behaves in a way that is not attractive",
+                "hello": "used when meeting or greeting someone",
+                "boy": "a male child or, more generally, a male of any age",
+                "girl": "a female child or young woman, especially one still at school",
+                }
+        return dict
+
+    def _Get_keylist(self, dict):
+        key_list = []
+        for key in dict.keys():
+            key_list.append(key)
+        return key_list
+
     def run(self):
         list = self._Base_List()
-        print(list)
-    
+        dict = self._Base_dict()
+        key_list = self._Get_keylist(dict)
+        random.shuffle(key_list)
+        key = key_list[0]
+        value = dict[key]
+        print(value)
+
+
 ##############################################################################
 
 def main():
     action = Action()
     action.run()
-    
+
 if __name__=="__main__":
     main()
