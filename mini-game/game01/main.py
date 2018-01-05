fbcdd5907a2620ff83b07db579ada9607b2db0e5
takumikaka <qk2006qk@163.com>
2018-01-05 14:29:00 +0800

    update mini-game game01

--- mini-game/game01/main.py
+++ mini-game/game01/main.py
@@ -12,12 +12,19 @@ def green(text):
 def yellow(text):
 	return '\033[93m{0}\033[00m'.format(text)
 #############################################################################
+'''
+1. record life
+2. get words and random.words
+3. record scores
+4. game start and game end
+'''
 
 class Action(object):
 
     def __init__(self):
         pass
 
+    # def Base_list
     def _Base_List(self):
         list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
                 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
@@ -25,7 +32,8 @@ class Action(object):
         ]
         return list
 
-    def _Base_dict(self):
+    # def Base_dict
+    def _Base_Dict(self):
         dict = {"dag": "a person who does not look attractive or who behaves in a way that is not attractive",
                 "hello": "used when meeting or greeting someone",
                 "boy": "a male child or, more generally, a male of any age",
@@ -33,18 +41,40 @@ class Action(object):
                 }
         return dict
 
-    def _Get_keylist(self, dict):
+    # get key_list
+    def _Get_KeyList(self, dict):
         key_list = []
         for key in dict.keys():
             key_list.append(key)
         return key_list
 
+    def _Change_KeytoList(self, key):
+        list = []
+        for i in key:
+            list.append(i)
+        return list
+
+    def _Random_BaseList(self, list):
+        list = random.sample(list, 5)
+        return list
+
+    def _Get_ChoicList(self, list1, list2):
+        list = list1 + list2
+        n = len(list)
+        randomlist = random.sample(list, n)
+        return randomlist
+
     def run(self):
         list = self._Base_List()
-        dict = self._Base_dict()
-        key_list = self._Get_keylist(dict)
+        dict = self._Base_Dict()
+        key_list = self._Get_KeyList(dict)
         random.shuffle(key_list)
         key = key_list[0]
+        changekeytolist = self._Change_KeytoList(key)
+        randombaselist = self._Random_BaseList(list)
+        choicelist = self._Get_ChoicList(changekeytolist, randombaselist)
+        print(choicelist)
+        '''
         value = dict[key]
         print(value)
         str = input("Enter the str:")
@@ -52,6 +82,7 @@ class Action(object):
             print("successful!")
         else:
             print("error")
+        '''
 
 
 ##############################################################################
