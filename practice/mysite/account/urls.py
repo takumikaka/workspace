6de50f485e202573e706af04bd6a987e11186976
takumikaka <qk2006qk@163.com>
2018-01-10 17:58:17 +0800

    新加了一个login页面
    ..../account/login

---
+++ practice/mysite/account/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url
+from . import views
+
+from django.conf import settings
+
+urlpatterns = [
+    url(r'^login/$', views.user_login, name='user_login'),
+]
--- practice/mysite/mysite/settings.py
+++ practice/mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
