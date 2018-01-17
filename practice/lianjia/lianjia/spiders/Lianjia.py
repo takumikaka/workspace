8b9f0df4ab5203afdeb369d58f0bb6a3b00e4dae
takumikaka <qk2006qk@163.com>
2018-01-17 21:39:15 +0800

    完成多页面信息抓取

--- practice/lianjia/lianjia/spiders/Lianjia.py
+++ practice/lianjia/lianjia/spiders/Lianjia.py
@@ -3,14 +3,24 @@ import scrapy
 from scrapy import Spider
 from scrapy.selector import Selector
 from lianjia.items import LianjiaItem
+from scrapy.spiders import Rule, CrawlSpider
+from scrapy.linkextractors import LinkExtractor
 
-class LianjiaSpider(scrapy.Spider):
+class LianjiaSpider(CrawlSpider):
     name = 'Lianjia'
-    allowed_domains = ['Lianjia.com']
-    url = 'https://cd.lianjia.com/ershoufang/c1611047140791/?sug=%E9%94%A6%E6%B1%9F%E5%9F%8E%E5%B8%82%E8%8A%B1%E5%9B%AD%E4%B8%89%E6%9C%9F'
+    #allowed_domains = ['Lianjia.com']
+    allowed_domains = []
+    #url = 'https://cd.lianjia.com/ershoufang/c1611047140791/?sug=%E9%94%A6%E6%B1%9F%E5%9F%8E%E5%B8%82%E8%8A%B1%E5%9B%AD%E4%B8%89%E6%9C%9F'
+    url = 'https://cd.lianjia.com/ershoufang/chuanshi'
     start_urls = [url]
 
-    def parse(self, response):
+    rules=(
+        Rule(LinkExtractor(allow=(r'https://cd.lianjia.com/ershoufang/chuanshi/pg\d')),callback='parse_item',follow=True),
+    )
+
+    #def parse(self, response):
+    def parse_item(self, response):
+        print('Test Info................')
         bodys = Selector(response).xpath("body//div[@class='clear']")
 
         for body in bodys:
