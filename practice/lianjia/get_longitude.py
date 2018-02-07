7b0b1e2c4328b2d1b9c1f9e121487a43171ef5ca
takumikaka <qk2006qk@163.com>
2018-02-07 21:25:13 +0800

    修改可以在3.x下运行及继续优化代码

--- practice/lianjia/get_longitude.py
+++ practice/lianjia/get_longitude.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
-#python2.x
 
-import urllib2
+import requests
 import sys
 from save_file import save_json
 import time
-reload(sys)
-sys.setdefaultencoding('utf8')
 
 def replace_blank(lists):
     for i in range(len(lists)):
@@ -16,17 +13,21 @@ def replace_blank(lists):
 def get_latitude(lists):
     try:
         for i in range(len(lists)):
-            add = lists[i].decode('utf-8')
+            add = lists[i]
+            print(add)
             i += 1
             print('The current progress is as follows: {0}/{1}'.format(i, len(lists)))
             time.sleep(0.5)
             print('Start get url {}...'.format(add))
             url = 'https://api.map.baidu.com/geocoder?address={}&city=成都市&output=json&key=1FPdGX4ZnhhBj4QEGyfsVBbkZM4kNWNM'.format(add)
             print('Start get latitude {}...'.format(add))
-            html = urllib2.urlopen(urllib2.Request(url))
-            json1 = html.read()
+            response = requests.get(url)
+            answer = response.json()
+            lng = answer['result']['location']['lng']
+            lat = answer['result']['location']['lat']
+            local = {'add': add, 'local': {'lng': lng, 'lat': lat}}
             time.sleep(0.5)
             print('Start save file...')
-            save_json(json1)
+            save_json(local)
     except Exception:
         print('Get latitude fail...')
