51048d5e8553910e3212db178859a4bfa16dd7dc
takumikaka <qk2006qk@163.com>
2018-02-22 22:10:51 +0800

    完成scrapy代码

--- practice/lianjia/test.py
+++ practice/lianjia/test.py
@@ -1,26 +1,35 @@
 # -*- coding: utf-8 -*-
 
+
 from get_longitude import replace_blank, get_latitude
 from read_file import read_file
 from collections import Counter
 from dict_file import get_count, dict_Tolist
+from get_mongodbValue import get_mongodbValue, remove_dict
+from save_file import save_json
 
-lists = ['龙湖晶蓝半岛一期 ', '蓉上坊一期 ', '卓锦城五期 ', '卓锦城六期 ', '沙河壹号二期 ', '澳龙名城 ',
-        '星河名都 ', '翡翠城四期 ', '万科金润华府 ', '锦江城市花园三期 ', '时代豪庭一期 ', '华韵天府 ',
-        '翡翠城三期 ', '四海逸家二期 ', '上海东韵 ', '蓝光凯丽香江 ', '摩根中心 ', '弘邦领邸 ', '绿地锦天府 ',
-        '仁恒滨河湾 ', '卓锦城一期 ', '比华利国际城一期 ', '新沙河阳光水岸 ', '比华利国际城一期 ', '卓锦城五期 ',
-        '星河名都 ', '蓉上坊一期 ', '锦江城市花园二期 ', '华都美林湾 ', '卓锦城六期 '
-        ]
+lists = get_mongodbValue()          # 取得mongodb community value
 
 lists = replace_blank(lists)        # 去掉空格
 
 list_dict = get_count(lists)        # 得到计数字典
+list_dict = remove_dict(list_dict)
+print(list_dict)
 
 dict_lists = list_dict.keys()       # 关键字列表
 value_lists = list_dict.values()    # 键值列表
 
 d_lists = dict_Tolist(dict_lists)   # 标准列表
 v_lists = dict_Tolist(value_lists)  # 标准列表
+
 get_latitude(d_lists, v_lists)      # 获得地址
 #content = read_file("latitude.json")
 #print('The json file is: {0}'.format(content))
+'''
+d_dict = {'卓锦城六期': 15, '四海逸家二期': 35, '卓锦城五期': 18, '蓉上坊一期': 11, '沙河壹号二期': 14, '澳龙名城': 28, '星河名都': 2, '翡翠城四期': 31, '万科金润华府': 34, '锦江城市花园三期': 43}
+print(d_dict)
+list_err = ['四海逸家二期']
+for i in list_err:
+    d_dict.pop(i)
+print(d_dict)
+'''
