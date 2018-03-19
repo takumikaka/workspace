01f61960bb6176e7297f6044ab6dd8160beec52b
takumikaka <qk2006qk@163.com>
2018-03-19 14:07:08 +0800

    成语识图第一版
    搭建基本框架，能够通过取得的字对比答案得到结果

---
+++ practice/idiom/main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+#from processers.random_lst import randomLst
+from processers.analyze_data import analyzeData
+
+version = 'V 0.0.1'
+
+analyze_data = analyzeData()
+analyze_data.run()
+
+print(version)
