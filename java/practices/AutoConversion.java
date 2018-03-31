ee774b1666c508446147e3087234492cdcaaf2be
takumikaka <qk2006qk@163.com>
2018-03-31 23:30:22 +0800

    更新疯狂java讲义练习

---
+++ java/practices/AutoConversion.java
@@ -0,0 +1,11 @@
+public class AutoConversion{
+    public static void main(String[] args){
+        int a = 6;
+        float f = a;
+        System.out.println(f);
+
+        byte b = 9;
+        double d = b;
+        System.out.println(d);
+    }
+}
---
+++ java/practices/PrimitiveAndString.java
@@ -0,0 +1,12 @@
+public class PrimitiveAndString{
+    public static void main(String[] args){
+        String str1 = "5";
+        System.out.println(str1);
+
+        String str2 = 3.5f + "";
+        System.out.println(str2);
+
+        System.out.println(3 + 4 + "Hello!");
+        System.out.println("Hello!" + 3 + 4);
+    }
+}
