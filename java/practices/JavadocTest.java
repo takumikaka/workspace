0a4a77125c0e69bf9c247dcf54a26ab476b77517
takumikaka <qk2006qk@163.com>
2018-03-31 18:47:43 +0800

    更换java学习目录 更新学习java资料

---
+++ java/practices/JavadocTest.java
@@ -0,0 +1,21 @@
+package takumi;
+/**
+* Description:
+* <br> Web URL: <a href='http://www.baidu.com'>baidu</a>
+* <br>Author: takumi kaka
+* <br>This program is protected by copyright laws.
+* @version: V0.0.1
+*/
+
+public class JavadocTest{
+    /**
+    * Simple Test for member value.
+    */
+    protected String name;
+    /**
+    *Main function
+    */
+    public static void main(String[] args){
+        System.out.println("Hello Word!");
+    }
+}
---
+++ java/practices/Test.java
@@ -0,0 +1,20 @@
+package keke;
+/**
+* Description:
+* <br> Web URL: <a href='http://www.baidu.com'>baidu</a>
+* <br>Author: takumi kaka
+* <br>This program is protected by copyright laws.
+* @version: V0.0.1
+*/
+
+public class Test{
+    /**
+    * Simple Test for member value.
+    */
+    public int age;
+    /**
+    *Test construction of Test class
+    */
+    public Test(){
+    }
+}
