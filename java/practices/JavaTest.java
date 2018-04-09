76d336a5c3cb38a08ac45de35634a084c3fe551c
takumikaka <qk2006qk@163.com>
2018-04-09 16:20:56 +0800

    更新疯狂java讲义练习

--- java/practices/JavaTest.java
+++ java/practices/JavaTest.java
@@ -1,15 +1,7 @@
 public class JavaTest{
     public static void main(String[] args){
-        String numStr = "123.456";
-        int lenStr = numStr.length();
-        System.out.println("The str len is: " + lenStr);
-        for(int i = 0; i < lenStr; i++){
-            int num = numStr.charAt(i) - 48;
-            System.out.println(num);
-            System.out.println("\u5341\u4E07");
-            if(i == lenStr - 1){
-                System.out.println(i);
-            }
-        }
+        double num = 123.456;
+        long zheng = (long)num;
+        System.out.println(zheng);
     }
 }
--- java/practices/Num2Rmb.java
+++ java/practices/Num2Rmb.java
@@ -1,18 +1,38 @@
 public class Num2Rmb{
     private String[] hanArr = {"\u96F6", "\u58F9", "\u8D30", "\u53C1", "\u8086", "\u4F0D", "\u9646", "\u67D2", "\u634C", "\u7396", "\u62FE"};
-    private String[] unitArr = {"\u5341", "\u767E", "\u5343", "\u4E07", "\u5341", "\u767E", "\u5343", "\u4EBF"};
-    private String[] divide(double num){
+    private String[] zheng_unitArr = {"\u5341", "\u767E", "\u5343", "\u4E07", "\u5341", "\u767E", "\u5343", "\u4EBF"};
+    private String[] xiao_unitArr = {"\u5206", "\u89D2"};
+    private String zhengDivide(double num){
+        long zheng = (long)num;
+        String zheng_num = Long.toString(zheng);
+        return zheng_num;
+    }
+    private String xiaoDivide(double num){
         long zheng = (long)num;
         long xiao = Math.round((num - zheng) * 100);
-        return new String[]{zheng + "", String.valueOf(xiao)};
+        String xiao_num = Long.toString(xiao);
+        return xiao_num;
     }
-    private String toHanStr(String numStr){
+    private String zhengToHanStr(String numStr){
         String result = "";
         int numLen = numStr.length();
         for(int i = 0; i < numLen; i++){
             int num = numStr.charAt(i) - 48;
             if(i != numLen - 1 && num != 0){
-                result += hanArr[num] + unitArr[numLen - 2- i];
+                result += hanArr[num] + zheng_unitArr[numLen - 2- i];
+            }else{
+                result += hanArr[num];
+            }
+        }
+        return result;
+    }
+    private String xiaoToHanStr(String numStr){
+        String result = "";
+        int numLen = numStr.length();
+        for(int i = 0; i < numLen; i++){
+            int num = numStr.charAt(i) - 48;
+            if(i != numLen - 1 && num != 0){
+                result += hanArr[num] + xiao_unitArr[numLen - 1 - i];
             }else{
                 result += hanArr[num];
             }
@@ -22,6 +42,10 @@ public class Num2Rmb{
     public static void main(String[] args){
         Num2Rmb nr = new Num2Rmb();
         //System.out.println(Arrays.toString(nr.divide(1234.56789)));
-        System.out.println(nr.toHanStr("1234567") + "\u5143");
+        String zheng_num = nr.zhengDivide(123.4567);
+        String xiao_num = nr.xiaoDivide(123.4567);
+        String zheng_han = nr.zhengToHanStr(zheng_num) + "\u5143";
+        String xiao_han = nr.xiaoToHanStr(xiao_num) + "\u5206";
+        System.out.println(zheng_han + xiao_han);
     }
 }
