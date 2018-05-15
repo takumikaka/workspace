91289f6f3c50168a976ee3d8ccbeb6b8238a335a
takumikaka <qk2006qk@163.com>
2018-05-15 20:11:46 +0800

    备份python

--- practice/CrazyAnswer/processers/answer_game.py
+++ practice/CrazyAnswer/processers/answer_game.py
@@ -12,8 +12,7 @@ class answerGame(object):
         self.lst_two = ['A', 'B', 'C']
         self.lst_answer_one = ['A', 'B', 'C', 'D']
         self.lst_answer_two = ['A', 'B', 'C']
-        self.lst_score_one = []
-        self.lst_score_two = []
+        self.lst_score = []
 
     def get_random(self):
         return random.randint(1, 1000)
@@ -44,27 +43,24 @@ class answerGame(object):
                 Flag = True
         return score
 
-    def get_dict_answer_one(self, lst_choice, lst_answer, dict, i, time_orig):
+    def get_dict_answer_one(self, lst_choice_one, lst_answer_one, lst_choice_two, lst_answer_two, dict, i, time_orig):
         if len(dict[i]) == 6:
             lst_option = [dict[i]['option1'], dict[i]['option2'], dict[i]['option3']]
             random.shuffle(lst_option)
-            dict_answer = self.get_answer(lst_choice, lst_option, 3)
+            dict_answer = self.get_answer(lst_choice_one, lst_option, 3)
             input_num = input("输入你的选项: ")
             time_one = int(time.time())
             time_result = time_one - time_orig
-            score_one = self.comp_result(input_num, lst_answer, dict_answer, dict, i, time_result)
-            return score_one
-
-    def get_dict_answer_two(self, lst_choice, lst_answer, dict, i, time_orig) :
-        if len(dict[i]) == 7:
+            score = self.comp_result(input_num, lst_answer_one, dict_answer, dict, i, time_result)
+        else:
             lst_option = [dict[i]['option1'], dict[i]['option2'], dict[i]['option3'], dict[i]['option4']]
             random.shuffle(lst_option)
-            dict_answer = self.get_answer(lst_choice, lst_option, 4)
+            dict_answer = self.get_answer(lst_choice_two, lst_option, 4)
             input_num = input("输入你的选项: ")
             time_two = int(time.time())
             time_result = time_two - time_orig
-            score_two = self.comp_result(input_num, lst_answer, dict_answer, dict, i, time_result)
-            return score_two
+            score = self.comp_result(input_num, lst_answer_two, dict_answer, dict, i, time_result)
+        return score
 
     def run(self):
         dict_data = self.read_json.run()
@@ -74,9 +70,6 @@ class answerGame(object):
             if dict_data[i]['id'] == str(num):
                 print(dict_data[i]['des'])
                 time_orig = int(time.time())
-                score_one = self.get_dict_answer_one(self.lst_two, self.lst_answer_two, dict_data, i, time_orig)
-                score_two = self.get_dict_answer_two(self.lst_one, self.lst_answer_one, dict_data, i, time_orig)
-                #lst_score_one = self.get_score.get_lst_score(score_one, self.lst_score_one)
-                #lst_score_two = self.get_score.get_lst_score(score_two, self.lst_score_two)
-        print(score_one,score_two)
-        #print(lst_score_one, lst_score_two)
+                score = self.get_dict_answer_one(self.lst_one, self.lst_two, self.lst_answer_one, self.lst_answer_two, dict_data, i, time_orig)
+
+        print(score)
